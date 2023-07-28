5603918af6965ae0852a826e2a01e26d46fe02ea
lincon-tech <65660810+lincon-tech@users.noreply.github.com>
2023-07-28 17:37:49 +0100

    Update Configuration_Template.tf

--- Configuration_Template.tf
+++ Configuration_Template.tf
@@ -4,7 +4,7 @@ resource "aws_launch_configuration" "web" {
 
   image_id      = data.aws_ssm_parameter.instance_ami.value
   instance_type = var.instance_type
-  key_name      = var.keyname
+  key_name      = var.Keyname
 
   security_groups             = ["${aws_security_group.ELB_SG.id}"]
   associate_public_ip_address = true
